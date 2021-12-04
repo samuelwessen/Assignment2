1979c86a064835914d3119a803071caf64fafd4f
Samuel Wessén <samuel.wessen@learnet.se>
2021-12-04 13:26:15 +0100

    StateCommandMemento klar de andra är ej klara

--- StateCommandMemento/SCMMain.cs
+++ StateCommandMemento/SCMMain.cs
@@ -11,12 +11,7 @@ namespace Assignment2.StateCommandMemento
 
         
 
-        /*
-         * Skapa en maskin som kan vara av eller på. !Använd State Pattern!
-         * Om man ger den ett kommando när den är av så ska den spara det kommandot och sen köra alla sparade kommandon när den sätts på. !Använd Command Pattern!
-         * Kommandot ska vara att skriva ut en sträng text som användaren anger.
-         * Det ska också finnas en reset funktion som tar bort alla sparade kommandon och stänger av maskinen. !Använd Memento Pattern!
-         */
+       
         public void Run()
         {
             var machine  = new Machine();
